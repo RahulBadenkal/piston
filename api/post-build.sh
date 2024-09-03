645cf9a937529b0ce9226ba7c1e8b19be2f3dc4e
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-09-03 22:20:39 +0530

    updated

--- api/post-build.sh
+++ api/post-build.sh
@@ -6,8 +6,9 @@ apt-get install curl
 
 # Define variables
 data_dir="/piston/packages"
-python_3_12_env_file="$data_dir/python/3.12.0/environment"
-node_20_env_file="$data_dir/node/20.11.1/environment"
+python_3_12_env_config_file="$data_dir/python/3.12.0/environment"
+python_3_12_env_file="$data_dir/python/3.12.0/.env"
+node_20_env_config_file="$data_dir/node/20.11.1/environment"
 
 rm -rf /piston/packages/*
 
@@ -19,13 +20,13 @@ if [ ! -f "$python_3_12_env_file" ]; then
          -d '{"language": "python", "version": "3.12.0"}'
 
     # Append the line to the environment file
+    echo "export PYTHONUNBUFFERED=1" >> "$python_3_12_env_config_file"
     echo "export PYTHONUNBUFFERED=1" >> "$python_3_12_env_file"
-    cat "$python_3_12_env_file"
 fi
 
 
 # Make sure node 20.11.1 is installed and configured
-if [ ! -f "$node_20_env_file" ]; then
+if [ ! -f "$node_20_env_config_file" ]; then
     # Make a POST request to the API
     curl -X POST http://localhost:2000/api/v2/packages \
          -H "Content-Type: application/json" \
